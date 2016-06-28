81c2af22ac54472e8e7dcbaff9ac25212e157cda
Jay W Johnson <jwj@boun.cr>
2016-06-28 13:15:07 +0800

    Revert to previous config
    Rename some "note" files

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const config = {
-  mongoURL: process.env.OPENSHIFT_MONGODB_DB_URL + 'tmcharts' || 'mongodb://localhost:27017/mern-starter',
-  port: process.env.OPENSHIFT_NODEJS_PORT || 8000,
+  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  port: process.env.OPENSHIFT_NODEJS_PORT || 80,
 };
 
 export default config;
